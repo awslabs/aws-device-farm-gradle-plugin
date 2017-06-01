469edc9017fca451f0520a0fd64c4a88831f033b
Rohan Deshpande <appwiz@users.noreply.github.com>
2017-06-01 12:44:11 -0700

    LICENSE and formatting updates (#17)
    o Updated Apache 2.0 license in each source file.
    o Fix imports and formatting.
    o Add gradle exclusions to gitignore.

--- aws-devicefarm-gradle-plugin/src/main/groovy/com.amazonaws.devicefarm/DeviceFarmClientFactory.java
+++ aws-devicefarm-gradle-plugin/src/main/groovy/com.amazonaws.devicefarm/DeviceFarmClientFactory.java
@@ -1,19 +1,17 @@
-/*
- * Copyright 2015 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+//
+// Copyright 2015-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
+// A copy of the License is located at
+//
+// http://aws.amazon.com/apache2.0
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
 package com.amazonaws.devicefarm;
 
 import com.amazonaws.ClientConfiguration;
@@ -57,7 +55,7 @@ public class DeviceFarmClientFactory {
 
         AWSDeviceFarmClient apiClient = new AWSDeviceFarmClient(credentials, clientConfiguration);
         apiClient.setServiceNameIntern("devicefarm");
-        if(extension.getEndpointOverride() != null) {
+        if (extension.getEndpointOverride() != null) {
             apiClient.setEndpoint(extension.getEndpointOverride());
         }
 
@@ -72,7 +70,7 @@ public class DeviceFarmClientFactory {
             props.load(DeviceFarmServer.class.getResourceAsStream("/META-INF/gradle-plugins/version.properties"));
             return props.getProperty("version");
 
-        } catch(IOException e) {
+        } catch (IOException e) {
             throw new DeviceFarmException("Unable to read version", e);
         }
     }
--- aws-devicefarm-gradle-plugin/src/main/groovy/com.amazonaws.devicefarm/extension/DeviceFarmExtension.groovy
+++ aws-devicefarm-gradle-plugin/src/main/groovy/com.amazonaws.devicefarm/extension/DeviceFarmExtension.groovy
@@ -1,18 +1,17 @@
-/*
- * Copyright 2015 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+//
+// Copyright 2015-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License").
+// You may not use this file except in compliance with the License.
+// A copy of the License is located at
+//
+// http://aws.amazon.com/apache2.0
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+//
 package com.amazonaws.devicefarm.extension
 
 import org.gradle.api.Project
@@ -51,7 +50,7 @@ class DeviceFarmExtension {
     String consoleUrl = "https://console.aws.amazon.com/devicefarm/home?#/projects/%s/runs/%s"
 
     /**
-    * User-Agent
+     * User-Agent
      */
     String userAgent = "AWS Device Farm - Gradle %s"
 
@@ -77,8 +76,8 @@ class DeviceFarmExtension {
 
     boolean isValid() {
         projectName != null &&
-        authentication.valid &&
-        test != null && test.valid
+                authentication.valid &&
+                test != null && test.valid
     }
 
     void useMeteredDevices() {
